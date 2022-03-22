26f2694671e89dff27d9af2a5e0e19aeb62aba44
agustin <agustin.iglesias@murciaeduca.es>
2022-03-22 16:47:11 +0100

    Gestión de Logs

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 
-
+from .log.logs import configure_logging
 
 app = Flask(__name__)
 
 
 from .public import public
+
+logger = configure_logging(__name__)
 
 def create_app():
     app.register_blueprint(public)
--- app/public/routes.py
+++ app/public/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template
 
+import app
 from . import public
+
 
 
 @public.route('/')
 def index():  # put application's code here
+    app.logger.info("Mensaje de información")
     return render_template('index.html')
